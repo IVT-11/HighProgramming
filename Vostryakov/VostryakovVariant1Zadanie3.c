adc1d4b414919708b7b62b710e04f6b901253a76
IVT-11 <55914488+IVT-11@users.noreply.github.com>
2019-11-09 17:18:40 +0300

    Update VostryakovVariant1Zadanie3.c

--- Vostryakov/VostryakovVariant1Zadanie3.c
+++ Vostryakov/VostryakovVariant1Zadanie3.c
@@ -1,16 +1,32 @@
 #include<stdio.h>
 #include<math.h>
-int main(){
+int main()
+{
 float a,b,c,k,S=0,n,x;
 printf("n= ");
 scanf("%f",&n);
 printf("\nx= ");
 scanf("%f",&x);
-for(k=1;k<n;k++){
+while(1)
+{
+if (n>0)
+break;
+else
+{
+printf("Не корректные значения введите другие значения");
+printf("\nn= ");
+scanf("%f",&n);
+printf("\nx= ");
+scanf("%f",&x);
+}
+}
+for(k=1;k<n;k++)
+{
 a=pow(-1,k-1);
 b=(k*k+k+1)/pow(x,k);
 c=a*b;
-S+=c;}
+S+=c;
+}
 printf("%f\n",S);
 return 0;
 }
