77f041cd6790e3908a2caadb39de38c5e8fef964
IVT-11 <55914488+IVT-11@users.noreply.github.com>
2019-11-09 14:01:43 +0300

    Кр2Вар4

--- Dunaykin/kr2zadanie3.c
+++ Dunaykin/kr2zadanie3.c
@@ -4,14 +4,14 @@ int main()
 {
 float k=0.0,x,N,S=0.0,a=0.0;
 printf("x=");
-scanf("%d",&x);
+scanf("%f",&x);
 for(k=0;abs(x)<1,abs(a)<=0.001;k++)
 {
 a=pow(-x,k)/(k*k+2);
 N=a;
 S+=pow(-x,k)/(k*k+2);
 }
-printf("%d",S);
+printf("%f",S);
 return 0;
 }
 
