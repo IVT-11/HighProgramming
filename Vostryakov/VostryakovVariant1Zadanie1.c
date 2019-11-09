857838dc95a916b77b39dfe4df77270da70c4c87
IVT-11 <55914488+IVT-11@users.noreply.github.com>
2019-11-09 17:45:55 +0300

    Add files via upload

--- Vostryakov/VostryakovVariant1Zadanie1.c
+++ Vostryakov/VostryakovVariant1Zadanie1.c
@@ -1,34 +1,35 @@
 #include<stdio.h>
 #include<math.h>
 int main()
-{float a,b,c,alfa,beta,gama,e;
+{
+float a,b,c,alfa,beta,gama,e;
 printf("Введите а= ");
 scanf("%f",&a);
 printf("\nВведите b= ");
 scanf("%f",&b);
 printf("\nВведите угол alfa= ");
 scanf("%f",&alfa);
-while(1){
-if (a>0 && b>0 && alfa>0)
+while(1)
+{
+if (a>0 && b>0 && alfa>0 && alfa<180)
 break;
-else{
+else
+{
 printf("Не корректные значения введите другие значения");
 printf("\na= ");
 scanf("%f",&a);
 printf("\nb= ");
 scanf("%f",&b);
 printf("\nугол alfa= ");
-scanf("%f",&alfa);}}
-if(alfa<180){
+scanf("%f",&alfa);
+}
+}
+c=sqrt(a*a+b*b-2*a*b*cos(alfa));
 e=(sin(alfa)*b)/a;
-beta=asin(e);
+beta=asin(e)/M_PI*180;
 if (beta<0)
 beta=beta*(-1);
 gama=180-alfa-beta;
-c=a*sin(gama)/sin(alfa);}
-if(c<0)
-c=c*(-1);
 printf("beta=%f gama=%f c=%f\n",beta,gama,c);
 return 0;
 }
-
