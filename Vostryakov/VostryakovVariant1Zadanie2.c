61f88b14c1c3cc4a60ea0795f4fd4414f7cf433a
IVT-11 <55914488+IVT-11@users.noreply.github.com>
2019-11-09 18:03:10 +0300

    Add files via upload

--- Vostryakov/VostryakovVariant1Zadanie2.c
+++ Vostryakov/VostryakovVariant1Zadanie2.c
@@ -1,17 +1,24 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 int main(){
-float x,y,a,b,c;
-printf("x= ");
-scanf("%f",&x);
-if (x>=2)
-printf("Нет корней\n");
-if (x>1 && x<2) {
-a=cos(x)*cos(x);
-printf("Функция =%f\n",a);}
-b=x/10;
-if (x<=b || b>=2){
-c=1+sin(x)*sin(x);
-printf("Функция =%f\n",c);}
+float x,y,b;
+printf("x=");
+scanf("%f", &x);
+b = x/10;
+if(x>1 && x<2){
+y = cos(x)*cos(x);
+printf("y=");
+printf("%f", y);
+}
+else{
+    if(b>=x || b>=2){
+y = 1+sin(x)*sin(x);
+printf("y=");
+printf("%f", y);
+}
+else{
+    printf("Error!");
+}
+}
 return 0;
 }
