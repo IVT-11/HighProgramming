f5cf1b853048e9bcc4ba0b98cc6ef25b969b7569
IVT-11 <55914488+IVT-11@users.noreply.github.com>
2019-11-10 01:13:40 +0300

    Add files via upload

---
+++ Yaltonskiy/KR2V21-3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int n, k = 1, k1 = 0;
+    float e = 2.71828, x, ak = 0, s;
+    
+    printf("Введите x: "); scanf("%f", &x);
+   
+    if (fabs(x) < 1)
+    {
+        printf("Введите другое значение x: "); scanf("%f", &x);
+    }
+    else
+        {
+         for (k1=0;1;k1++)
+            {
+            if (fabs(ak)<0.01)
+	            {
+	            ak = (pow(e, ((k * x) / 2))) / k;
+	            }
+            else
+	            {
+	            n=k1;
+	            break;
+	            }
+            }
+        }
+    for (k = 1; k <= n; k++)
+        {
+            s = pow(e, ((k * x) / 2)) / k;
+        }
+        
+    printf("Сумма равна: %.2f", s);
+
+    return 0;
+}
