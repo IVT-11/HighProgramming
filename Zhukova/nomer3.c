9d55dd6cf8432f8dc499f860e70a985bae264764
IVT-11 <55914488+IVT-11@users.noreply.github.com>
2019-11-09 19:35:45 +0300

    Добавила Контрольная 2 Вариант 5
    У меня есть сомнения что само задание задано верно. Чисто математически если выбрать такой х, при котором а-нное будет <=0.01, при вычислении следующих членов последовательности каждый будет все меньше и меньше и цикл будет бесконечным. Не из-за того, что цикл написан неправильно и из него не прописан выход,а потому что в задаче что-то не так с условием.

--- Zhukova/nomer3.c
+++ Zhukova/nomer3.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
 int main()
 {
 float P=1.0,a=0.0;
@@ -8,6 +5,9 @@ double x;
 int k=1, z=1;
 
 printf("Введите значение х=\n");scanf("%lf",&x);
+a=(sin(k*x))/z;
+if(fabs(a)<=0.01)
+{
 while(fabs(a)<=0.01)
  {
   z*=2*k-1;
@@ -15,10 +15,12 @@ while(fabs(a)<=0.01)
   P*=a;
   k++;
  }
+}
+else printf("Введено неверное значение х\n");
+ 
 if(k>=1)
 {
-printf("Геометрическая последовательность равна %f\n",P);
-printf("N=%d\n",k);
+printf("N=%d\n",k-1);
  }
 else
 printf("Решений нет\n");
