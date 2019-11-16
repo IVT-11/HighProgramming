2bfde1c202eeb39387dcfb2313c4b7dd56cab547
IVT-11 <55914488+IVT-11@users.noreply.github.com>
2019-11-16 09:13:36 +0300

    Update and rename zadanie 3.c to zadanie 3_v19.c

--- skopich/zadanie 3_v19.c
+++ skopich/zadanie 3_v19.c
@@ -7,7 +7,7 @@ int main()
  scanf("%d",&x);
  Ak=(1/pow(2,k))*sin(x)/pow(x,k+1);
  for(k=1;1;k++)
- if(abs(Ak/s)<0.0001)
+ if(fabs(Ak/s)<0.0001)
  Ak=(1/pow(2,k))*sin(x)/pow(x,k+1);
  else
   {
@@ -16,7 +16,7 @@ int main()
 }
  
 k=1;
-for(k;n;k++)
+for(k;k<=n;k++)
 {
 s+=(1.0/pow(2,k))*sin(x)/pow(x,k+1);
 printf("%f\n",s);
