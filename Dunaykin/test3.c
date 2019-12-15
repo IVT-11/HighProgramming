e476a96eddf427fd7af1d2965cd65f3578cd5186
IVT-11 <55914488+IVT-11@users.noreply.github.com>
2019-12-15 13:50:21 +0300

    Контрольная работа №3. Вариант №4.

--- Dunaykin/test3.c
+++ Dunaykin/test3.c
@@ -2,13 +2,13 @@
 
 #include <math.h>
 
-float function(float,float);
+double function(double,double);
 
-float function(float a, float b)
+double function(double a,double b)
 
 {
 
- float f;
+ double f;
 
  f=(sin(2*a)+cos(2*a)/sin(a))*(a*a+b*b);
 
@@ -22,10 +22,17 @@ int main()
 
 double x,y;
 printf("x\t     y\t     f(x,y)\t\n\n",x,y,function(x,y));
-for(x=-1.0;x<=1.0;x+=0.25)
+for(x=-1;x<=1;x+=0.25)
 {
-    for(y=0.0;y<=2.0;y+=0.1)
-printf("%.3lf     %.3lf     %.3lf\n",x,y,function(x,y));
+    for(y=0;y<=2;y+=0.1)
+	{
+		if(x==0)
+			printf("%.3lf	  %.3lf	    Деление на ноль\n",x,y);
+		else
+		{
+			printf("%.3lf     %.3lf     %.3lf\n",x,y,function(x,y));
+		}
+	}
 }
 return 0;
 }
