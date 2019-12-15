dfc2a162ec496e0978c648064ff4d77f455aaf5a
IVT-11 <55914488+IVT-11@users.noreply.github.com>
2019-12-15 14:42:28 +0300

    Добавила тест 3 Вар  5

--- Zhukova/test3_var5.c
+++ Zhukova/test3_var5.c
@@ -10,17 +10,19 @@ float func(float a, float b)
 int main()
 {
 float x=0,y=-0.5, c;
+printf("x\t y\t f(x,y)\n\n");
 while(x<=2)
 {
 while(y<=2)
 {
- printf("%.3f\t",func(x,y));
+ if((expf(x)+expf(-x))!=0&&(expf(y)-expf(-y))!=0&&(x+y)/2<=1&&(x+y)/2>=-1)
+ printf("%.3f, %.3f, %.3f\n",x, y, func(x,y));
+ else if((expf(x)+expf(-x))==0||(expf(y)-expf(-y))==0||(x+y)/2>1&&(x+y)/2<-1)
+ printf("%.3f, %.3f, происходит деление или умножение на 0\n", x, y);
  y+=0.1;
 }
-printf("\n");
 y=-0.5;
 x+=0.25;
 } 
 return 0;
 }
-
