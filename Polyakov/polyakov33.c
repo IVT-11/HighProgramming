416d6badc4eef09c5cb78f993e7daae90bdc5fac
IVT-11 <55914488+IVT-11@users.noreply.github.com>
2019-12-15 19:19:27 +0300

    Контрольная-3 вариант 18
    Добавил контрольную-3 вариант 18

--- Polyakov/polyakov33.c
+++ Polyakov/polyakov33.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <math.h>
+float func(float,float);
+float func(float a,float b)
+{
+float c;
+c=(a*log(a)+b*log(b))/(pow(a,3)-pow(b,3));
+return c;
+}
 int main()
 {
-float a;
-printf("x,y,f(x,y)");
-for(float x=1.0;x<=2.0;x+=0.1)
+float c;
+printf("x\t y\t f(x,y)\t\n");
+for(float x=1.0;x<=2;x+=0.1)
 {
-for(float y=-2.0;y<=2.0;y+=0.5) 
+for(float y=-2.0;y<=2;y+=0.5) 
 {
-a=(x*log(x)+y*log(y))/(pow(x,3)-pow(y,3));
-printf("%.3f  %.3f   %.3f\n",x,y,a);
+printf("%.3f  %.3f   %.3f\n",x,y,func(x,y));
 }
 }
 return 0;
