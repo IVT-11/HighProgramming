1f1de19c648bd9085571dcde3520daebc0325c18
IVT-11 <55914488+IVT-11@users.noreply.github.com>
2019-12-19 19:42:40 +0300

    Update kr.c

--- Zinakov/kr.c
+++ Zinakov/kr.c
@@ -10,12 +10,12 @@ int main()
   {
   for (double y = 0; y <= 2; y += 0.25)
    {
-    res = exp(x) / (pow(x, 2) + pow(y, 2) - 1.0);
-     if ((pow(x,2) + pow(y,2) - 1.0) == 0.0)
-       printf("%.2f\t%.1f\tdiv by 0\n", x, y);
+    res = log10(x-y)/pow(x,2)+pow(y,2)+x+y+1;
+     if ((pow(x,2)+pow(y,2)+x+y+1) == 0.0 || x==y || x<y)
+       printf("%.2f\t%.2f\t0.000\n", x, y);
      else
        {
-       printf("%.2f\t%.1f\t%.3f\n", x, y, res);
+       printf("%.2f\t%.2f\t%.3f\n", x, y, res);
        }
    }
   }
